901e9640b3d82dc32790a7caad1a2de01930acf0
btangmu <thomasbishop@wenlin.com>
2020-03-06 11:22:46 -0500

    CLDR-13585 Move more Survey Tool code from jsp to java files
    -Remove js_include.jsp, move code to new SurveyAjax.includeJavaScript()
    
    -New boolean INCLUDE_SCRIPT_VERSION, false for now, see CLDR-13582
    
    -Comments

--- tools/cldr-apps/src/org/unicode/cldr/web/SurveyAjax.java
+++ tools/cldr-apps/src/org/unicode/cldr/web/SurveyAjax.java
@@ -3102,8 +3102,8 @@ public class SurveyAjax extends HttpServlet {
 
         out.write(prefix + "jquery.autosize.min.js" + tail);
 
-        out.write(prefix + "survey.js" + v + tail);
         out.write(prefix + "CldrStAjax.js" + v + tail);
+        out.write(prefix + "survey.js" + v + tail);
         out.write(prefix + "CldrSurveyVettingLoader.js" + v + tail);
         out.write(prefix + "CldrSurveyVettingTable.js" + v + tail);
 
