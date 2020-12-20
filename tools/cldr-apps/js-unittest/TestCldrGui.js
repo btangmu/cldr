60abfa988cfd25339a2f573641299e5b5672b1df
btangmu <thomasbishop@wenlin.com>
2020-12-19 22:38:35 -0500

    CLDR-14251 Modernize: remove v.jsp; add SurveyTool.java, CldrGui.js
    -New SurveyTool.java with most of the Java from v.jsp
    
    -New CldrGui.js with most of the HTML from v.jsp
    
    -Remove SurveyAjax.includeEvilJavaScript; handle more such items with CldrStatus.js
    
    -Move SurveyAjax.includeJavaScript and related code to SurveyTool.java
    
    -Extend statusJSON, new class StatusForFrontEnd to include more info needed by front end
    
    -More js unit tests; encapsulate parseAsMimeType in new TestCldrTest.js
    
    -Change dojo config parseOnLoad to false, instead use dojo/parser in cldrGui.run
    
    -In redesign/review.js, use dojo/ready instead of jquery equivalent: wait for cldrGui.run
    
    -Skip dojo/ready where superfluous
    
    -Use XMLHttpRequest instead of dojo/request in cldrStAjax, per vanilla-js.com
    
    -Rename dojo dependencies in showV for clarity, e.g. dojoxBusyButton for BusyButton
    
    -Normalize error handlers for cldrStAjax.sendXhr: err parameter is always a string
    
    -New CldrText.js to replace stui.str and stui.sub; no more dojo/string or loadStui
    
    -New WHAT_ADMIN_PANEL so front end does not need vap for URL if logged in as admin
    
    -Restore/revise comment about fonts in surveytool.css (removed in PR 877)
    
    -Fix CLDR-14365 by calling hideRightPanel from loadHandler for Date, Zones, Numbers reports
    
    -Run npx prettier on all .js files except those that are minified
    
    -Remove dead code; clean up
    
    -Comments

--- tools/cldr-apps/js-unittest/TestCldrGui.js
+++ tools/cldr-apps/js-unittest/TestCldrGui.js
@@ -1,28 +1,31 @@
-'use strict';
+"use strict";
 
 {
-	const assert = chai.assert;
+  const assert = chai.assert;
 
-    describe('cldrGui.test.getBodyHtml', function() {
-        const html = cldrGui.test.getBodyHtml();
-    
-        it('should not return null or empty', function() {
-            assert((html != null && html !== ''), "html is neither null nor empty");
-        });
+  describe("cldrGui.test.getBodyHtml", function () {
+    const html = cldrGui.test.getBodyHtml();
 
-        const xml = '<body>' + html + '</body>';
-        const xmlStr = cldrTest.parseAsMimeType(xml, 'application/xml')
-        it('should return valid xml when in body element', function() {
-            assert(xmlStr || false, 'parses OK as xml when in body element');
-        });
-
-        const htmlStr = cldrTest.parseAsMimeType(html, 'text/html');
-        it('should return good html', function() {
-            assert(htmlStr || false, 'parses OK as html');
-        });
-
-        it('should contain angle brackets', function() {
-            assert((htmlStr.indexOf('<') !== -1) && (htmlStr.indexOf('>') !== -1), 'does contain angle brackets');
-        });
+    it("should not return null or empty", function () {
+      assert(html != null && html !== "", "html is neither null nor empty");
     });
+
+    const xml = "<body>" + html + "</body>";
+    const xmlStr = cldrTest.parseAsMimeType(xml, "application/xml");
+    it("should return valid xml when in body element", function () {
+      assert(xmlStr || false, "parses OK as xml when in body element");
+    });
+
+    const htmlStr = cldrTest.parseAsMimeType(html, "text/html");
+    it("should return good html", function () {
+      assert(htmlStr || false, "parses OK as html");
+    });
+
+    it("should contain angle brackets", function () {
+      assert(
+        htmlStr.indexOf("<") !== -1 && htmlStr.indexOf(">") !== -1,
+        "does contain angle brackets"
+      );
+    });
+  });
 }
