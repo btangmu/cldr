58c534215cb8257ce84248b027c2838916a4b700
btangmu <thomasbishop@wenlin.com>
2026-04-10 13:20:53 -0400

    CLDR-18759 Do not use inherited empty exemplar set as a candidate item
    -Skip inherited item if it is only empty brackets and path contains exemplarCharacters
    
    -Encapsulate brackets as UnicodeSetPrettyPrinter.SET_OPEN/SET_CLOSE/EMPTY_SET

--- tools/cldr-code/src/main/java/org/unicode/cldr/util/UnicodeSetPrettyPrinter.java
+++ tools/cldr-code/src/main/java/org/unicode/cldr/util/UnicodeSetPrettyPrinter.java
@@ -26,6 +26,10 @@ import java.util.TreeSet;
  * For the Survey Tool, should use SimpleUnicodeSetFormatter.java
  */
 public class UnicodeSetPrettyPrinter implements FormatterParser<UnicodeSet> {
+    public static final char SET_OPEN = '[';
+    public static final char SET_CLOSE = ']';
+    public static final String EMPTY_SET = "[]";
+
     private static final StringComparator CODEPOINT_ORDER =
             new UTF16.StringComparator(true, false, 0);
     private static final UnicodeSet PATTERN_WHITESPACE =
@@ -37,7 +41,7 @@ public class UnicodeSetPrettyPrinter implements FormatterParser<UnicodeSet> {
             new UnicodeSet("[\\[\\]\\-\\^\\&\\\\\\{\\}\\$\\:]").addAll(PATTERN_WHITESPACE).freeze();
 
     private boolean first = true;
-    private StringBuffer target = new StringBuffer();
+    private final StringBuffer target = new StringBuffer();
     private int firstCodePoint = -2;
     private int lastCodePoint = -2;
     private boolean compressRanges = true;
@@ -290,8 +294,8 @@ public class UnicodeSetPrettyPrinter implements FormatterParser<UnicodeSet> {
             return this;
         }
         switch (codePoint) {
-            case '[': // SET_OPEN:
-            case ']': // SET_CLOSE:
+            case SET_OPEN:
+            case SET_CLOSE:
             case '-': // HYPHEN:
             case '^': // COMPLEMENT:
             case '&': // INTERSECTION:
